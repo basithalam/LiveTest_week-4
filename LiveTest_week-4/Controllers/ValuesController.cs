0552d0fb7c8f0245e87abf6eafe543c0cd2bd296
basithalam <cognitivebd.com@gmail.com>
2025-10-05 21:05:29 +0600

    Add project files.

---
+++ LiveTest_week-4/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiveTest_week_4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        // ✅ GET: api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var values = new List<string> { "value1", "value2", "value3" };
+            return Ok(values);
+        }
+    }
+}
